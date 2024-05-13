41a1c1b0ee63b7b21c0fed751583271ac947aee6
KhasanboyAkhmedov <paragon7400@gmail.com>
2024-05-13 19:15:28 +0500

    explore

--- components/shared/follow-bar.tsx
+++ components/shared/follow-bar.tsx
@@ -16,11 +16,13 @@ const FollowBar = () => {
       <div className="bg-neutral-800 rounded-xl p-4">
         <div className="flex items-center justify-between">
           <h2 className="text-white text-xl font-semibold">Who to follow</h2>
-          <Button
-            secondary
-            label={"See all"}
-            classNames="h-[30px] p-0 w-fit px-3 text-sm"
-          />
+          <Link href="/explore">
+            <Button
+              secondary
+              label={"See all"}
+              classNames="h-[30px] p-0 w-fit px-3 text-sm"
+            />
+          </Link>
         </div>
 
         {isLoading ? (
